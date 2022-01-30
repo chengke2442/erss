ff675a20b0131c0a64193352feab36e214848a00
xutingWu01 <xuting0227@outlook.com>
2022-01-30 16:22:30 -0500

    add setting

--- web-app/mysite/settings.py
+++ web-app/mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hw1',
         'USER': 'xw218',
-        'PASSWORD': '',
+        'PASSWORD': 'WXTwxt20UNNC!',
         'HOST': '127.0.0.1',
         'PORT': '5432',
 
